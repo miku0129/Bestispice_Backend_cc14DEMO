e59b592883a8253457b59ef65a472f8e830d7a8a
miku <svartkatt39@gmail.com>
2020-09-30 16:58:50 +0900

    initialize db in app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db
 
 
 def create_app(env_name):
@@ -15,6 +16,8 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+
+    db.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
