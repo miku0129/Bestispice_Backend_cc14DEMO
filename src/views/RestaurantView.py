59d2ebe942d8e2f6636b79bff7d6a498cbc74279
miku <svartkatt39@gmail.com>
2020-10-01 01:22:29 +0900

    made endpoint - get, post, del, put

--- src/views/RestaurantView.py
+++ src/views/RestaurantView.py
@@ -24,12 +24,12 @@ def create_info():
 
 
 @restaurant_api.route('/<int:restaurant_id>', methods=['DELETE'])
-# delete a restaurant
+# delete a restaurant which find by id
 def delete(restaurant_id):
     restaurant = RestaurantsModel.get_one_restaurant(restaurant_id)
     restaurant.delete()
 
-    return custom_response({'message': 'deleted'}, 201)
+    return custom_response({'message': 'Deleted'}, 201)
 
 
 @restaurant_api.route('/<int:restaurant_id>', methods=['GET'])
@@ -38,7 +38,7 @@ def get_an_info(restaurant_id):
     ret = RestaurantsModel.get_one_restaurant(restaurant_id)
     print('ret', ret.name)
     # return custom_response({'message': 'done!'}, 201)
-    return jsonify(name=ret.name, feature=ret.feature, place=ret.place)
+    return jsonify(name=ret.name, feature=ret.feature, place=ret.place, tell=ret.tell, business_hours1=ret.business_hours1, business_hours2=ret.business_hours2, regular_holiday=ret.regular_holiday, url=ret.url)
 
 
 @restaurant_api.route('/', methods=['GET'])
@@ -53,9 +53,22 @@ def get_all_info():
     # for item in ret:
     #     jsonify(name=item.name)
     # ret_dct = {i: ret[i].name for i in range(0, len(ret), 1)}
-    ret_dct = {i: [ret[i].name, ret[i].feature]for i in range(0, len(ret), 1)}
+
+    # ret_dct = {i: [ret[i].name, ret[i].feature]for i in range(0, len(ret), 1)}
+    ret_dct = {ret[i].name: [ret[i].id, ret[i].feature,
+                             ret[i].place, ret[i].url]for i in range(0, len(ret), 1)}
 
     return custom_response(ret_dct, 201)
+
+
+@restaurant_api.route('/<int:restaurant_id>', methods=['PUT'])
+# update information of single restaurants which be found by id
+def update(restaurant_id):
+    req_data = request.get_json()
+    data = req_data
+    ret = RestaurantsModel.get_one_restaurant(restaurant_id)
+    ret.update(data)
+    return jsonify(name=ret.name, feature=ret.feature, place=ret.place, tell=ret.tell, business_hours1=ret.business_hours1, business_hours2=ret.business_hours2, regular_holiday=ret.regular_holiday, url=ret.url)
 
 
 def custom_response(res, status_code):
