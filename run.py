c91563fa4cb7dbba6e127f515603607540616afd
miku <svartkatt39@gmail.com>
2020-10-01 12:02:25 +0900

    change port setting

--- run.py
+++ run.py
@@ -7,4 +7,6 @@ if __name__ == '__main__':
     env_name = os.getenv('FLASK_ENV')
     app = create_app(env_name)
     # run app
-    app.run()
+    # app.run()
+    app.run(debug=False, host='0.0.0.0',
+            port=int(os.environ.get('PORT', 5000)))
